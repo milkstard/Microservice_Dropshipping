dd5534083a3586655197a5553fa56a2af06f4c74
milkstard <jmarkcarbs@gmail.com>
2025-08-22 07:41:07 +0800

    Initial login FE implementation

---
+++ Dropshipping_FE/dropsubuy_fe/src/app/pages/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+
+@Component({
+  selector: 'dbuy-login',
+  standalone: true,
+  imports: [MatSelectModule, MatInputModule, MatFormFieldModule, CommonModule, ReactiveFormsModule, FormsModule, MatToolbarModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  public loginDetails: FormGroup = new FormGroup({});
+  public matcher = new MyErrorStateMatcher();
+  constructor() {}
+
+  ngOnInit() {
+    this.loginDetails = new FormGroup({
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+  
+}
