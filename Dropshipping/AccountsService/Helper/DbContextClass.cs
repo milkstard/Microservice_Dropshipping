2659f2c5d94f2069030a6d3d64308bcc15dedd30
milkstard <jmarkcarbs@gmail.com>
2025-11-07 00:01:49 +0800

    Implement fastapi

--- Dropshipping/AccountsService/Helper/DbContextClass.cs
+++ Dropshipping/AccountsService/Helper/DbContextClass.cs
@@ -19,10 +19,30 @@ namespace AccountsService.Helper
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Users>()
-                .HasIndex(u => u.Email)
+            //modelBuilder.Entity<Users>()
+            //    .HasIndex(u => u.Email)
+            //    .IsUnique();
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasIndex(u => u.Email)
                 .IsUnique();
 
+                entity.HasOne(u => u.UserType)
+                      .WithMany(t => t.Users)
+                      .HasForeignKey(u => u.UserTypeFK);
+
+                entity.Property(u => u.UserTypeFK)
+                      .HasColumnName("UserType_FK");
+
+                entity.HasOne(u => u.UserSalt)
+                      .WithOne(t => t.User)
+                      .HasForeignKey<Users>(u => u.UserSaltId);
+
+                entity.Property(u => u.UserSaltId)
+                      .HasColumnName("UserSaltID_FK");
+            });
+                
         }
         public DbSet<Users> Users { get; set; }
         public DbSet<UserSalt> UserSalts { get; set; }
